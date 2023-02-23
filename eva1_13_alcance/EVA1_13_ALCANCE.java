6afe6c7253b58efb942d3b1179995088de0ca49c
EduardoMunozN <124702009+EduardoMunozN@users.noreply.github.com>
2023-02-23 09:39:32 -0600

    Add files via upload

---
+++ eva1_13_alcance/EVA1_13_ALCANCE.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_13_alcance;
+
+/**
+ *
+ * @author eduar
+ */
+public class EVA1_13_ALCANCE {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+                System.out.println("i = " +i);
+                int j = 100;//a partir de aquí es visible la letra j
+            
+        }//termina ciclo for
+    }
+}
